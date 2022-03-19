beea1bd5e655df63c3324d709e4252b490484238
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-03-20 00:13:59 +1100

    Finalised design for chat rooms

--- src/Chat.js
+++ src/Chat.js
@@ -43,29 +43,50 @@ function Chat(props) {
     <div className='chat'>
         <div className='chat--nav'>
             <h2>Your other rooms</h2>
-            <SignOut setAuthenticated={setAuthenticated}/>
-            <button className='button--black' type="submit" onClick={() => {setChooseRoom(true)}}>Join a new room</button>
+            <div className='chat--nav--rooms'>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+                <p>keggers</p>
+            </div>
+            <div className='chat--nav--btns'>
+                <SignOut setAuthenticated={setAuthenticated}/>
+                <button className='button--black' type="submit" onClick={() => {setChooseRoom(true)}}>Join a new room</button>
+            </div>
         </div>
         <div className='chat--room'>
             <div className='chat--room--container'>
-                {roomMessages != null &&
-                    Object.keys(roomMessages).reverse().map((messageData, index) => 
-                    <ChatBubble
-                        key = {index}
-                        uid = {props.uid}
-                        senderUid = {roomMessages[messageData].uid}
-                        profilePic = {roomMessages[messageData].profile__pic} 
-                        userName = {roomMessages[messageData].sender}
-                        message = {roomMessages[messageData].message}
-                        time = {roomMessages[messageData].timestamp}
-                    /> 
-                    )
-                }
-                <div className='chat--room--container--heading'>
+                    <div className='chat--room--container--chats'>
+                        {roomMessages != null &&
+                            Object.keys(roomMessages).reverse().map((messageData, index) => 
+                            <ChatBubble
+                                key = {index}
+                                uid = {props.uid}
+                                senderUid = {roomMessages[messageData].uid}
+                                profilePic = {roomMessages[messageData].profile__pic} 
+                                userName = {roomMessages[messageData].sender}
+                                message = {roomMessages[messageData].message}
+                                time = {roomMessages[messageData].timestamp}
+                            /> 
+                            )
+                        }
+                    </div>
+                    <div className='chat--room--container--heading'>
                     <h2>{props.room}</h2>
-                    {props.room != "main" &&
-                        <p>In this room:</p>
-                    }
+                        {props.room != "main" &&
+                            <div className='chat--room--container--heading--members'>
+                                <h3>In this room:</h3>
+                                <div>Benjamin Bialy, Ben Naaj, HShah HAH, JJSjaa anana, Bsahaj ahaHH, Ben Naaj, HShah HAH, JJSjaa anana, Bsahaj ahaHH, Ben Naaj, HShah HAH, JJSjaa anana, Bsahaj ahaHH</div>
+                            </div>
+                        }
                 </div>
             </div>
             <form className="chat--room--send" onSubmit={(e) => { handleAddingMsg(e) }}>

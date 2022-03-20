1a59e3b0fe4c0d9ec91a1f056a9bfd1c1dd7bbbc
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-03-20 16:47:17 +1100

    styling and components for ChooseRoom.js

---
+++ src/UsersRooms.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./UsersRooms.css"
+
+function UsersRooms(props) {
+    const { setChooseRoom } = props;
+  return (
+    <div className='users__rooms'>
+        <div className='users__rooms--heading'>
+            <h4>{props.roomName}</h4>
+            <p>Created by: {props.roomCreator}</p>
+        </div>
+        <div className='users__rooms--members'>
+          <p>Members: {props.members}</p>
+        </div>
+        <button className="users__rooms--join button" type="submit" onClick={() => {setChooseRoom(props.roomName)}}>Join</button>
+    </div>
+  )
+}
+
+export default UsersRooms

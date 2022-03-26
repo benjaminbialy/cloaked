7a1bca2ddb20a831bb67a6141d331aa31508b4e5
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-03-26 22:31:34 +1100

    added reacts, styling required now

--- src/Chat.js
+++ src/Chat.js
@@ -1,136 +1,175 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 import { getDatabase, ref, push, set, onValue } from "firebase/database";
-import ChatBubble from './ChatBubble';
-import SignOut from './SignOut';
-import "./Chat.css"
+import ChatBubble from "./ChatBubble";
+import SignOut from "./SignOut";
+import "./Chat.css";
 
 function Chat(props) {
+	const { setChooseRoom, setAuthenticated } = props;
 
-    const { setChooseRoom, setAuthenticated } = props;
+	const [message, setMessage] = useState("");
+	const [roomMessages, setRoomMessages] = useState([]);
 
-    const [message, setMessage] = useState("");
-    const [roomMessages, setRoomMessages] = useState([])
+	const db = getDatabase();
+	const chatroomRef = ref(db, "chatrooms/" + props.room + "/messages");
 
-    const db = getDatabase();
-    const chatroomRef = ref(db, 'chatrooms/' + props.room + "/messages");
+	const handleAddingMsg = (e) => {
+		e.preventDefault();
+		const newChatroomRef = push(chatroomRef);
 
+		set(newChatroomRef, {
+			message: message,
+			sender: props.userName,
+			profile__pic: props.profilePic,
+			uid: props.uid,
+			timestamp: Date.now(),
+			reply: false,
+		});
+		setMessage("");
+	};
 
-    const handleAddingMsg = (e) => {
-        e.preventDefault();
-        const newChatroomRef = push(chatroomRef);
+	useEffect(() => {
+		onValue(chatroomRef, (snapshot) => {
+			const data = snapshot.val();
+			setRoomMessages(roomMessages);
+			setRoomMessages(data);
+		});
+	}, [props.room]);
 
-        set(newChatroomRef, {
-            message: message,
-            sender: props.userName,
-            profile__pic: props.profilePic,
-            uid: props.uid,
-            timestamp: Date.now()
-        });
-        setMessage("")
-    }
+	if (props.room != "main") {
+		// used to get the index of roomDataArray element with the field of roomName == attemptName
+		let indexOfRoomID = props.arrUsersRoomsData.findIndex(
+			(id) => id.roomName == props.room
+		);
 
-    useEffect(() => {
-        onValue(chatroomRef, (snapshot) => {
-            const data = snapshot.val();
-            setRoomMessages(roomMessages)
-            setRoomMessages(data)
-        });
-    }, [props.room])
+		var roomMembers = [];
 
+		// gets all of the members out of a room in array form
+		for (
+			let i = 0;
+			i < props.arrUsersRoomsData[indexOfRoomID].members.length;
+			i++
+		) {
+			// if there's only one member, don't add a comma on the end
+			if (props.arrUsersRoomsData[indexOfRoomID].members.length === 1) {
+				roomMembers.push(props.arrUsersRoomsData[indexOfRoomID].members[i]);
+			} else {
+				// if this is the last member in the array, don't add a comma
+				if (i != props.arrUsersRoomsData[indexOfRoomID].members.length - 1) {
+					roomMembers.push(
+						props.arrUsersRoomsData[indexOfRoomID].members[i] + ", "
+					);
+				} else {
+					roomMembers.push(props.arrUsersRoomsData[indexOfRoomID].members[i]);
+				}
+			}
+		}
+	}
 
-    if (props.room != "main"){
-        // used to get the index of roomDataArray element with the field of roomName == attemptName
-        let indexOfRoomID = props.arrUsersRoomsData.findIndex(id => id.roomName == props.room);
-
-        var roomMembers = []
-
-        // gets all of the members out of a room in array form
-        for (let i = 0; i < props.arrUsersRoomsData[indexOfRoomID].members.length; i++){
-            // if there's only one member, don't add a comma on the end
-            if (props.arrUsersRoomsData[indexOfRoomID].members.length === 1){
-                roomMembers.push(props.arrUsersRoomsData[indexOfRoomID].members[i])
-            }
-            else{
-                // if this is the last member in the array, don't add a comma
-                if(i != props.arrUsersRoomsData[indexOfRoomID].members.length - 1){
-                    roomMembers.push(props.arrUsersRoomsData[indexOfRoomID].members[i] + ", ")
-                }
-                else{
-                    roomMembers.push(props.arrUsersRoomsData[indexOfRoomID].members[i])
-                }
-            }
-        }
-    }
-
-  return (
-    <div className='chat'>
-        <div className='chat--nav'>
-            <h2>Your rooms</h2>
-            <div className='chat--nav--rooms'>
-                <button className="chat--nav--rooms--item" onClick={(e)=> setChooseRoom("main")}>main</button>
-            { props.arrUsersRoomsData !== null && props.arrUsersRoomsData !== "" &&
-                    props.arrUsersRoomsData.map((roomInfo, index) => (
-                        <button
-                            className="chat--nav--rooms--item"
-                            onClick={(e)=> setChooseRoom(roomInfo.roomName)}
-                            key = {index}
-                        >
-                            {roomInfo.roomName}    
-                        </button>
-                    ))
-                }
-            </div>
-            <div className='chat--nav--btns'>
-                <button className='button--black' type="submit" onClick={() => {setChooseRoom(true)}}>Join a new room</button>
-                <SignOut setAuthenticated={setAuthenticated}/>
-            </div>
-        </div>
-        <div className='chat--room'>
-            <div className='chat--room--top'>
-                <button className='button--black' type="submit" onClick={() => {setChooseRoom(true)}}>Join a new room</button>
-                <SignOut setAuthenticated={setAuthenticated}/>
-            </div>
-            <div className='chat--room--container'>
-                    <div className='chat--room--container--chats'>
-                        {roomMessages != null &&
-                            Object.keys(roomMessages).reverse().map((messageData, index) => 
-                            <ChatBubble
-                                key = {index}
-                                uid = {props.uid}
-                                senderUid = {roomMessages[messageData].uid}
-                                profilePic = {roomMessages[messageData].profile__pic} 
-                                userName = {roomMessages[messageData].sender}
-                                message = {roomMessages[messageData].message}
-                                time = {roomMessages[messageData].timestamp}
-                            /> 
-                            )
-                        }
-                    </div>
-                    <div className='chat--room--container--heading'>
-                    <h2>{props.room}</h2>
-                        {props.room != "main" &&
-                            <div className='chat--room--container--heading--members'>
-                                <h3>In this room:</h3>
-                                <div>{roomMembers}</div>
-                            </div>
-                        }
-                </div>
-            </div>
-            <form className="chat--room--send" onSubmit={(e) => { handleAddingMsg(e) }}>
-                <input required 
-                    type="text" 
-                    value={message} 
-                    placeholder='type your message' 
-                    onChange={(e)=> setMessage(e.target.value)}
-                    className='chat--room--send--input'
-                >
-                </input>
-                <input className='chat--room--send--submit' type="submit" value="Send"></input>
-            </form> 
-        </div>
-    </div>
-  )
+	return (
+		<div className="chat">
+			<div className="chat--nav">
+				<h2>Your rooms</h2>
+				<div className="chat--nav--rooms">
+					<button
+						className="chat--nav--rooms--item"
+						onClick={(e) => setChooseRoom("main")}
+					>
+						main
+					</button>
+					{props.arrUsersRoomsData !== null &&
+						props.arrUsersRoomsData !== "" &&
+						props.arrUsersRoomsData.map((roomInfo, index) => (
+							<button
+								className="chat--nav--rooms--item"
+								onClick={(e) => setChooseRoom(roomInfo.roomName)}
+								key={index}
+							>
+								{roomInfo.roomName}
+							</button>
+						))}
+				</div>
+				<div className="chat--nav--btns">
+					<button
+						className="button--black"
+						type="submit"
+						onClick={() => {
+							setChooseRoom(true);
+						}}
+					>
+						Join a new room
+					</button>
+					<SignOut setAuthenticated={setAuthenticated} />
+				</div>
+			</div>
+			<div className="chat--room">
+				<div className="chat--room--top">
+					<button
+						className="button--black"
+						type="submit"
+						onClick={() => {
+							setChooseRoom(true);
+						}}
+					>
+						Join a new room
+					</button>
+					<SignOut setAuthenticated={setAuthenticated} />
+				</div>
+				<div className="chat--room--container">
+					<div className="chat--room--container--chats">
+						{roomMessages != null &&
+							Object.keys(roomMessages)
+								.reverse()
+								.map((messageData) => (
+									<ChatBubble
+										key={messageData}
+										id={messageData}
+										uid={props.uid}
+										senderUid={roomMessages[messageData].uid}
+										profilePic={roomMessages[messageData].profile__pic}
+										userName={roomMessages[messageData].sender}
+										message={roomMessages[messageData].message}
+										time={roomMessages[messageData].timestamp}
+										reacts={roomMessages[messageData].reactions}
+										thisUserName={props.userName}
+										room={props.room}
+									/>
+								))}
+					</div>
+					<div className="chat--room--container--heading">
+						<h2>{props.room}</h2>
+						{props.room != "main" && (
+							<div className="chat--room--container--heading--members">
+								<h3>In this room:</h3>
+								<div>{roomMembers}</div>
+							</div>
+						)}
+					</div>
+				</div>
+				<form
+					className="chat--room--send"
+					onSubmit={(e) => {
+						handleAddingMsg(e);
+					}}
+				>
+					<input
+						required
+						type="text"
+						value={message}
+						placeholder="type your message"
+						onChange={(e) => setMessage(e.target.value)}
+						className="chat--room--send--input"
+					></input>
+					<input
+						className="chat--room--send--submit"
+						type="submit"
+						value="Send"
+					></input>
+				</form>
+			</div>
+		</div>
+	);
 }
 
-export default Chat
+export default Chat;
--- src/ChatBubble.js
+++ src/ChatBubble.js
@@ -1,71 +1,244 @@
-import React from 'react'
-import "./ChatBubble.css"
+import React, { useState } from "react";
+import { getDatabase, ref, update } from "firebase/database";
+import "./ChatBubble.css";
 
 function ChatBubble(props) {
+	const [messageClick, setMessageClick] = useState(false);
+	const [reactClick, setReactClick] = useState(false);
 
-  var time = new Date(props.time);
+	var time = new Date(props.time);
 
-  function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
-  
-  function formatDate(date) {
-    let daily = ""
-    let minutes = padTo2Digits(date.getMinutes())
-    let hours = date.getHours()
-    let day = ""
-    let month = ""
-    let year = ""
+	function padTo2Digits(num) {
+		return num.toString().padStart(2, "0");
+	}
 
-    if (hours > 12) {
-      daily = (hours - 12) + ":" + minutes + " pm"
-    }
-    else if (hours < 12) {
-      daily = hours + ":" + minutes + " am"
-    }
-    else{
-      daily = hours + ":" + minutes + " pm"
-    }
-    
-    day = padTo2Digits(date.getDate())
-    month = padTo2Digits(date.getMonth() + 1)
-    year = date.getFullYear()
+	function formatDate(date) {
+		let daily = "";
+		let minutes = padTo2Digits(date.getMinutes());
+		let hours = date.getHours();
+		let day = "";
+		let month = "";
+		let year = "";
 
-    return daily + " " + day + "-" + month + "-" + year
-  }
-    
-  return (
-    <div className='chat__bubble'
-      style={{
-        alignSelf: props.senderUid === props.uid ? 'flex-end' : 'flex-start',
-        flexDirection: props.senderUid === props.uid ? 'row' : 'row-reverse',
-        margin: props.senderUid === props.uid ? "0.5% 0 0.5% 50px" : "0.5% 50px 0.5% 0" 
+		if (hours > 12) {
+			daily = hours - 12 + ":" + minutes + " pm";
+		} else if (hours < 12) {
+			daily = hours + ":" + minutes + " am";
+		} else {
+			daily = hours + ":" + minutes + " pm";
+		}
 
-      }}
-    >
-      <div className='chat__bubble--text'
-            style={{
-              backgroundColor: props.senderUid === props.uid ? 'white' : '#EDEFF1'
-            }}
-      >
-        <div className='chat__bubble--text--send__info'>
-          <div>{props.senderUid === props.uid ? "You" : props.userName}</div>
-          {props.userName != "Admin" &&
-            <div className='chat__bubble--text--send__info--timestamp'>{formatDate(time)}</div>
-          }
-        </div>
-        <div>{props.message}</div>
+		day = padTo2Digits(date.getDate());
+		month = padTo2Digits(date.getMonth() + 1);
+		year = date.getFullYear();
 
-      </div>
-      {props.userName != "Admin" &&
-        <img
-          className='chat__bubble--profile__pic'
-          src={props.profilePic}
-          alt="profile pic"
-        />
-      }
-    </div>
-  )
+		return daily + " " + day + "-" + month + "-" + year;
+	}
+
+	const db = getDatabase();
+	const messageRef = ref(
+		db,
+		"chatrooms/" + props.room + "/messages/" + props.id
+	);
+
+	const sendReact = (reaction) => {
+		// all reacts for a message
+		let sendReactArray = [];
+
+		if (props.reacts !== undefined) {
+			// add all the existing objects into sendReactArray
+			for (let i = 0; i < props.reacts.length; i++) {
+				sendReactArray.push(props.reacts[i]);
+			}
+		}
+		// new reaction
+		let newReact = {};
+
+		newReact = {
+			reactSender: props.thisUserName,
+			react: reaction,
+		};
+
+		sendReactArray.push(newReact);
+
+		update(messageRef, { reactions: sendReactArray });
+
+		setReactClick(!reactClick);
+		setMessageClick(!messageClick);
+		console.log(reaction);
+	};
+
+	const messageClicked = () => {
+		setReactClick(!reactClick);
+		setMessageClick(!messageClick);
+	};
+
+	return (
+		<div
+			className="chat__bubble--container"
+			style={{
+				alignSelf: props.senderUid === props.uid ? "flex-end" : "flex-start",
+			}}
+		>
+			<div
+				className="chat__bubble"
+				style={{
+					flexDirection: props.senderUid === props.uid ? "row" : "row-reverse",
+				}}
+			>
+				<div
+					className="chat__bubble--options"
+					style={{
+						visibility: messageClick === true ? "visible" : "hidden",
+						flexDirection:
+							props.senderUid === props.uid ? "row" : "row-reverse",
+					}}
+				>
+					<button>reply</button>
+				</div>
+				<div
+					className="chat__bubble--text"
+					style={{
+						backgroundColor:
+							props.senderUid === props.uid ? "white" : "#EDEFF1",
+						marginLeft: props.userName === "Admin" ? "15px" : "0",
+					}}
+					onClick={(e) => {
+						messageClicked();
+					}}
+				>
+					<div className="chat__bubble--text--send__info">
+						<div>{props.senderUid === props.uid ? "You" : props.userName}</div>
+						{props.userName != "Admin" && (
+							<div className="chat__bubble--text--send__info--timestamp">
+								{formatDate(time)}
+							</div>
+						)}
+					</div>
+					<div>{props.message}</div>
+				</div>
+				{props.userName != "Admin" && (
+					<img
+						className="chat__bubble--profile__pic"
+						src={props.profilePic}
+						alt="profile pic"
+					/>
+				)}
+			</div>
+
+			{reactClick == true ? (
+				<div className="chat__bubble--container--reacts">
+					<div className="chat__bubble__container--reacts--container">
+						<span
+							onClick={(e) => {
+								sendReact("😂");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="laugh"
+						>
+							😂
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("😑");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="mid"
+						>
+							😑
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("😍");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="love"
+						>
+							😍
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("😘");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="kiss"
+						>
+							😘
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("😡");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="angry"
+						>
+							😡
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("😢");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="sad"
+						>
+							😢
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("🤑");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="money"
+						>
+							🤑
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("😬");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="awkward"
+						>
+							😬
+						</span>
+						<span
+							onClick={(e) => {
+								sendReact("😵‍💫");
+							}}
+							className="emoji"
+							role="img"
+							aria-label="stunned"
+						>
+							😵‍💫
+						</span>
+					</div>
+				</div>
+			) : props.reacts !== undefined ? (
+				<div className="chat__bubble--container--reacts">
+					<div className="chat__bubble__container--reacts--container">
+						<div class="tooltip">
+							😑
+							<span class="tooltiptext">Benjamin Bialy</span>
+						</div>
+						<div class="tooltip">
+							😑
+							<span class="tooltiptext">Benjamin Bialy</span>
+						</div>
+					</div>
+				</div>
+			) : (
+				<div className="nothing"></div>
+			)}
+		</div>
+	);
 }
 
-export default ChatBubble
+export default ChatBubble;
